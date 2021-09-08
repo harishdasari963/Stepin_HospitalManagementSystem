42e0bcf05540c8e9831f0aeb1bc8efcfe8d87b7c
Dasari Harsih <54410419+harishdasari963@users.noreply.github.com>
2021-09-08 08:34:15 +0530

    Update functions.h

--- 3_Implementation/inc/functions.h
+++ 3_Implementation/inc/functions.h
@@ -4,7 +4,7 @@
 #include<stdio.h>                   //contains printf,scanfetc
 //#include<conio.h>                   //contains delay(),getch(),gotoxy(),etc.
 #include<string.h>                  //contains strcmp(),strcpy(),strlen(),etc
-#include <windows.h>
+//#include <windows.h>
 #include<time.h>
 #include <ctype.h>
 //list of function prototype
